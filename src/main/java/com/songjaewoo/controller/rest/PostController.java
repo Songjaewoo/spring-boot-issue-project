6a0e3a6f23e781ddd8b57fac67a942e1c0342463
Administrator <Administrator@192.168.35.170>
2017-05-03 22:22:25 +0900

    test

--- src/main/java/com/songjaewoo/controller/rest/PostController.java
+++ src/main/java/com/songjaewoo/controller/rest/PostController.java
@@ -1,4 +1,4 @@
-package com.songjaewoo.controller;
+package com.songjaewoo.controller.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
